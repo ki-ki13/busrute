8498e383f26ea6addb91804ab8b58fd828e95feb
ki-ki13 <rizkimahjati845@students.unnes.ac.id>
2021-07-19 14:44:12 +0700

    need free mysql server hosting :(

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 // require_once ('MysqliDb.php');
 
 //$mysqli = new mysqli("localhost", "root", "", "busbantul");
-$mysqli = new mysqli("remotemysql.com", "a8XiESBMcr", "pwaiVweVXe", "a8XiESBMcr");
+$mysqli = new mysqli("sql4.freemysqlhosting.net", "sql4426367", "EiFJBUjHEW", "sql4426367");
 if($mysqli->connect_error) {
   exit('Could not connect');
 }
