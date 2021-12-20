513f3fdf0ae2b2775caecf28d599a6955d1868b3
Rizki Mahjati <56854278+ki-ki13@users.noreply.github.com>
2021-12-20 11:07:42 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,7 @@
 <?php
 // require_once ('MysqliDb.php');
 
-//$mysqli = new mysqli("localhost", "root", "", "busbantul");
-$mysqli = new mysqli("sql305.epizy.com", "epiz_29283373", "0c64kAKLcoZQ", "epiz_29283373_busbantul");
+$mysqli = new mysqli("localhost", "root", "", "busbantul");
 if($mysqli->connect_error) {
   exit('Could not connect');
 }
